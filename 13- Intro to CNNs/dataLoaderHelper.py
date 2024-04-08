e78bd829c99ac7d2c45c44b421a4576a44c909a0
Chaitanya Garg <98468427+EternalBlissard@users.noreply.github.com>
2024-04-08 17:34:20 +0530

    Added CIFAR10 DataLoader

--- 13- Intro to CNNs/dataLoaderHelper.py
+++ 13- Intro to CNNs/dataLoaderHelper.py
@@ -57,3 +57,56 @@ def getDataLoadersMNIST(batchSize, numWorkers=0,validFraction =None, trainTransf
     return trainLoader,testLoader
   else:
     return trainLoader,valLoader,testLoader
+  
+def getDataLoadersCIFAR10(batchSize, numWorkers=0,validFraction =None, trainTransforms =None, testTransforms =None ):
+  if(trainTransforms is None):
+    trainTransforms = transforms.ToTensor()
+  if(testTransforms is None):
+    testTransforms = transforms.ToTensor()
+
+  trainDataset = datasets.CIFAR10(root='data',
+                              train=True,
+                              transform=trainTransforms,
+                              download = True)
+
+  testDataset = datasets.CIFAR10(root='data',
+                              train=True,
+                              transform=testTransforms)
+
+  testDataset = datasets.CIFAR10(root='data',
+                              train=False,
+                              transform=testTransforms)
+
+  if(validFraction is not None):
+    num = int(validFraction*60000)
+    trainIndices = torch.arange(0,60000-num)
+    valIndices = torch.arange(60000-num, 60000)
+
+    trainSampler=SubsetRandomSampler(trainIndices)
+    valSampler = SubsetRandomSampler(valIndices)
+
+    trainLoader = DataLoader(dataset=trainDataset,
+                         batch_size=batchSize,
+                         num_workers=numWorkers,
+                         drop_last = True,
+                         sampler = trainSampler)
+    valLoader = DataLoader(dataset=trainDataset,
+                         batch_size=batchSize,
+                         num_workers=numWorkers,
+                         sampler = valSampler)
+  else:
+    trainLoader = DataLoader(dataset=trainDataset,
+                         batch_size=batchSize,
+                         num_workers=numWorkers,
+                         drop_last = True,
+                         shuffle=True)
+
+  testLoader = DataLoader(dataset=testDataset,
+                         batch_size=batchSize,
+                         shuffle=False,
+                         num_workers=numWorkers)
+
+  if(validFraction is None):
+    return trainLoader,testLoader
+  else:
+    return trainLoader,valLoader,testLoader
